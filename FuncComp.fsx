afc335a37d2fd0ae749a6d6234bc8d9c5b5d7243
Amol Dharmadhikari <adharmad@ionate.io>
2022-01-01 23:26:20 -0800

    Functional composition

--- FuncComp.fsx
+++ FuncComp.fsx
@@ -3,7 +3,17 @@
 
 open System
 
-seq { 0..100 }
-   |> Seq.filter (fun n -> n%2 <> 0)
-   |> Seq.sum
-   |> printfn "The sum of odd numbers from 0 to 100 is %A"
+let f0 x = x * 2
+let f1 x = x + 7
+
+// Compose a function g1(x) = f1(f0(x)) = 2x + 7
+let g1 = f0 >> f1
+
+// Compose a function g2(x) = f0(f1(x)) = 2x + 14
+let g2 = f0 << f1
+
+let res1 = g1 2
+let res2 = g2 2
+
+printfn "g1(2) = %i" res1
+printfn "g2(2) = %i" res2
